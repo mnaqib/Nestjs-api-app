67725e946e5f90285633cafe57f797cfa74cccb4
Mohammed Naqib <naqibdargaroad@gmail.com>
2022-07-17 02:41:25 +0530

    Deleted unwanted data

--- src/data.ts
+++ src/data.ts
@@ -19,28 +19,3 @@ interface Data {
 export const data: Data = {
   reports: [],
 };
-
-data.reports.push({
-  id: '1',
-  source: 'salary',
-  amount: 62300,
-  createdAt: new Date(),
-  updatedAt: new Date(),
-  type: ReportType.INCOME,
-});
-data.reports.push({
-  id: '2',
-  source: 'Mubashir',
-  amount: 5000,
-  createdAt: new Date(),
-  updatedAt: new Date(),
-  type: ReportType.INCOME,
-});
-data.reports.push({
-  id: '3',
-  source: 'Vehicle Loan',
-  amount: 26500,
-  createdAt: new Date(),
-  updatedAt: new Date(),
-  type: ReportType.EXPENSE,
-});
